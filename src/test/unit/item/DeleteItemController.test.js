91200fc64c4a78724b01533f74982229a527133e
Alejandro.F.Forero <54719974+AlejandroFonseca25@users.noreply.github.com>
2023-06-05 14:41:42 -0500

    Added unit tests for item CRUD (#15)

---
+++ src/test/unit/item/DeleteItemController.test.js
@@ -0,0 +1,26 @@
+import { expect } from 'chai';
+import moxios from 'moxios';
+const { StatusCodes } = require('http-status-codes');
+const ItemController =  require('../../../controller/ItemController').ItemController;
+const axiosInstance =  require('../../../controller/ItemController').axiosInstance;
+
+describe('Delete Item Controller Unit Tests', () => {
+  beforeEach(async () => {
+    moxios.install(axiosInstance);
+  });
+
+  it('Should delete an item successfully', async () => {
+    // Arrange
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 204,
+      });
+    });
+    // Act
+    const actualResponse = await ItemController.deleteItem(1);
+
+    // Assert
+    expect(actualResponse.status).to.equal(StatusCodes.NO_CONTENT);
+  })
+})
