a57ad650f59c67f3b3c74ee9098072ad3ce74089
Alejandro.F.Forero <54719974+AlejandroFonseca25@users.noreply.github.com>
2023-06-06 02:50:22 -0500

    Changed controller baseUrl

--- src/controller/ItemController.js
+++ src/controller/ItemController.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 
-const axiosInstance = axios.create({baseURL: process.env.API_URL})
+const axiosInstance = axios.create({baseURL: 'https://shopping-list-back.azurewebsites.net'})
 
 const ItemController = {
   createItem (nameParam, commentParam, listIdParam) {
