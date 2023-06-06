388c6fcef92680d6674ecea5d051354c70326efa
Cpasuy <carolinapasuy@hotmail.com>
2023-06-06 03:12:24 -0500

    Teste the url api

---
+++ cypress/e2e/test.cy.ts
@@ -0,0 +1,7 @@
+describe("Verifying the creation of a item", () => {
+
+
+    it("should create a item successfully with name and comment",  () => {
+        assert.equal("1", "1", "The item was not added");
+    });
+});
